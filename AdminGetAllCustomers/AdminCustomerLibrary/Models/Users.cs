5a2fc88eb0388d756e74de365c000d09c08e7231
Isabel Haataja <isabelhaataja@gmail.com>
2024-11-04 14:57:15 +0100

    Added a temporary User model and started on UserProvider.

---
+++ AdminGetAllCustomers/AdminCustomerLibrary/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿
+namespace AdminCustomerLibrary.Models;
+
+public class Users
+{
+    public int Id { get; set; }
+    public string UserType { get; set; }
+    public int Status { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public int PhoneNumber { get; set; }
+    public string UserName { get; set; }
+}
---
+++ AdminGetAllCustomers/AdminCustomerLibrary/UserProvider.cs
@@ -0,0 +1,29 @@
+﻿
+using AdminCustomerLibrary.Models;
+using System.Net.Http.Json;
+
+namespace AdminCustomerLibrary;
+
+public class UserProvider
+{
+    private readonly HttpClient _httpClient;
+
+    public UserProvider(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Users>> GetAllCustomersAsync()
+    {
+        try
+        {
+            var users = await _httpClient.GetFromJsonAsync<List<Users>>("api/Customer/GetAll");
+            return users ?? new List<Users>();
+        }
+        catch (Exception ex) 
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            return new List<Users>();
+        }
+    }
+}
