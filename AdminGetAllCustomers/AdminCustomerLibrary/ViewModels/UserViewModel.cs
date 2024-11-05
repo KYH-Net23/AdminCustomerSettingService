baaf691a3e744aa43259ad1979222ec5e500e153
Didi <demi.torrealba@hotmail.com>
2024-11-05 15:51:43 +0100

    test

--- AdminGetAllCustomers/AdminCustomerLibrary/ViewModels/UserViewModel.cs
+++ AdminGetAllCustomers/AdminCustomerLibrary/ViewModels/UserViewModel.cs
@@ -11,4 +11,5 @@ public class UserViewModel
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
     public string UserName { get; set; }
+    public string UserName { get; set; }
 }
